1814e7199f2623a9be5047ee5cafdbd0f3ba717c
NoahNrt <nrtkonto@gmail.com>
2026-08-30 23:33:17 +0200

    add feat auth

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Penis")
+from dotenv import load_dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
+    client_id=os.getenv("SPOTIFY_CLIENT_ID"),
+    client_secret=os.getenv("SPOTIFY_CLIENT_SECRET"),
+    redirect_uri=os.getenv("SPOTIFY_REDIRECT_URI"),
+    scope="user-library-read",
+))
+
+taylor_uri = 'spotify:artist:06HL4z0CvFAxyc27GXpf0'
+
+results = sp.artist_albums(taylor_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = sp.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
